2405b3ddc65a6c4828db5876b58e3fb2daf49981
Viktor Solovyev <barn-mih@rambler.ru>
2022-10-29 18:52:53 +0300

    refactor: remove tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.className="task-item";
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -37,13 +38,14 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-item__checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-input task-item__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
